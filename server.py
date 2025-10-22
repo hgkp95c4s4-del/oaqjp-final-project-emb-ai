7a2b9c9597d925bf74743d4b79ba51ffd5b30cba
jc <hgkp95c4s4@privaterelay.appleid.com>
2025-10-22 14:33:25 -0400

    flask 2

--- server.py
+++ server.py
@@ -6,3 +6,24 @@ app = Flask("Emotion Detector")
 def run_app():
     app.run(host="localhost", port="5000")
 
+@app.route("/emotionDetector")
+def detect_emotion():
+    input = request.args.get("textToAnalyze")
+    response = emotion_detector(input)
+
+    try:
+        output = f"For the given statement, the system response is 'anger': " + response['anger'],"
+                f" 'disgust': " + response['disgust'] + f"'fear': " + response['fear'] + f" 'joy': "
+                + response['joy'] + f" 'sadness':" + response['sadness'] + f". The dominant emotion is "
+                + response['dominant_emotion']
+    except:
+        output = "Failure in detection, something is wrong..."
+    finally:
+        continue
+
+@app.route("/")
+def render_home:
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    run_app()
